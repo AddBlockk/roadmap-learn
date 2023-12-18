61993034fa3b0c15bc8f9dae9e3842184fd639d8
Пользователь <09.09.2005@inbox.ru>
2023-12-19 00:47:25 +0400

    add ChangeTitle and Counter

---
+++ src/components/Counter.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import MyComponent from "./СhangeTitle";
+
+export default function Counter() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return (
+    <div className="container">
+      <div className="container-calc">
+        <button onClick={() => setCount(count * 0)} className="counter">
+          Reset
+        </button>
+        <button onClick={handleClick} className="counter">
+          +
+        </button>
+        <button className="counter">Значение: {count}</button>
+        <button onClick={() => setCount(count - 1)} className="counter">
+          -
+        </button>
+        <button onClick={() => setCount(count / 2)} className="counter">
+          /
+        </button>
+        <button onClick={() => setCount(count * 2)} className="counter">
+          *
+        </button>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/СhangeTitle.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from "react";
+
+export default function ChangeTitle() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = `Название заголовка: ${count}`;
+  }, [count]);
+
+  return (
+    <div className="change__title">
+      <button onClick={() => setCount((count) => count + 1)}>
+        Изменить Заголовок
+      </button>
+      <p className="comment">
+        // При нажатии на кнопку будет изменяться заголовок сайта, так что не
+        мудри
+      </p>
+    </div>
+  );
+}

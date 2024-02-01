ee3aa637125c6572f6d7154a896dcf6dd364de59
Boltach228 <08.08.2005@inbox.ru>
2024-02-01 19:02:27 +0400

    add text search

--- src/pages/Posts.tsx
+++ src/pages/Posts.tsx
@@ -6,6 +6,14 @@ const PostsStyled = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  h1 {
+    margin-bottom: 20px;
+  }
+  .input__search {
+    border-radius: 5px;
+    border: 1px solid black;
+    padding: 7px;
+  }
 `;
 
 interface PostData {
@@ -20,6 +28,7 @@ const Posts: React.FC = () => {
   const [posts, setPosts] = useState<PostData[]>([]);
   const [error, setError] = useState<string>("");
   const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [searchText, setSearchText] = useState<string>("");
 
   useEffect(() => {
     (async () => {
@@ -34,6 +43,22 @@ const Posts: React.FC = () => {
     })();
   }, []);
 
+  const filteredPosts = posts.filter((post) => {
+    const lowerCaseSearchText = searchText.toLowerCase();
+    const titleIncludesSearchText = post.title
+      .toLowerCase()
+      .includes(lowerCaseSearchText);
+    const bodyIncludesSearchText = post.body
+      .toLowerCase()
+      .includes(lowerCaseSearchText);
+    const numberIncludesSearchText = post.id.toString().includes(searchText);
+    return (
+      titleIncludesSearchText ||
+      bodyIncludesSearchText ||
+      numberIncludesSearchText
+    );
+  });
+
   if (error) {
     return <h1>Error: {error}</h1>;
   }
@@ -41,10 +66,19 @@ const Posts: React.FC = () => {
   return (
     <PostsStyled>
       <h1>Посты</h1>
+      <input
+        type="text"
+        className="input__search"
+        value={searchText}
+        onChange={(e) => setSearchText(e.target.value)}
+        placeholder="Поиск по тексту или числу"
+      />
       {isLoading ? (
         <h1>Загрузка...</h1>
+      ) : filteredPosts.length > 0 ? (
+        filteredPosts.map((post) => <Post key={post.id} {...post} />)
       ) : (
-        posts && posts.map((post) => <Post key={post.id} {...post} />)
+        <h2>Нет результатов поиска</h2>
       )}
     </PostsStyled>
   );
